3aeb36d017d149ea8f40081502079a487f1fa0de
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-05-13 15:31:25 +0530

    wallpaper-auto-select: show random effects

--- config/hypr/UserScripts/WallpaperSelect.sh
+++ config/hypr/UserScripts/WallpaperSelect.sh
@@ -14,7 +14,7 @@ iDIRi="$HOME/.config/swaync/icons"
 
 # swww transition config
 FPS=60
-TYPE="any"
+TYPE="random"
 DURATION=2
 BEZIER=".43,1.19,1,.4"
 SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
