1268449e4baf24f0b1e85503dcf4a1992db0d90e
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-07-17 00:37:04 +0530

    polkit-nixos: change to polkit-pantheon

--- config/hypr/scripts/Polkit-NixOS.sh
+++ config/hypr/scripts/Polkit-NixOS.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# For NixOS starting of polkit-gnome. Dec 2023, the settings stated in NixOS wiki does not work so have to manual start it
+# For NixOS starting of polkit-pantheon. Dec 2023, the settings stated in NixOS wiki does not work so have to manual start it
+# Modified by: JohnRTitor to work with polkit-pantheon
 
 # Find all polkit-gnome executables in the Nix store
-polkit_gnome_paths=$(find /nix/store -name 'polkit-gnome-authentication-agent-1' -type f 2>/dev/null)
+polkit_pantheon_paths=$(find /nix/store -name 'io.elementary.desktop.agent-polkit' -type f 2>/dev/null)
 
-for polkit_gnome_path in $polkit_gnome_paths; do
+for polkit_pantheon_path in $polkit_pantheon_paths; do
   # Extract the directory containing the executable
-  polkit_gnome_dir=$(dirname "$polkit_gnome_path")
+  polkit_pantheon_dir=$(dirname "$polkit_pantheon_path")
 
   # Check if the executable is valid and exists
-  if [ -x "$polkit_gnome_dir/polkit-gnome-authentication-agent-1" ]; then
-    # Start the Polkit-GNOME Authentication Agent
-    "$polkit_gnome_dir/polkit-gnome-authentication-agent-1" &
+  if [ -x "$polkit_pantheon_dir/io.elementary.desktop.agent-polkit" ]; then
+    # Start the Polkit-Pantheon Authentication Agent
+    "$polkit_pantheon_dir/io.elementary.desktop.agent-polkit" &
     exit 0
   fi
 done
 
 # If no valid executable is found, report an error
-echo "No valid Polkit-GNOME Authentication Agent executable found."
+echo "No valid Polkit-Pantheon Authentication Agent executable found."
